765cc190c8fd92b89ff2385ec5d7e897e12840b9
Savanna Denega Machado PERSONAL <savanna.denega@hotmail.com>
2018-06-17 09:42:27 -0300

    Desenvolvimento parcial do CRUD do menu Projetos

--- src/main/java/com/abc/abcontrol/repository/ProjetoRepository.java
+++ src/main/java/com/abc/abcontrol/repository/ProjetoRepository.java
@@ -19,16 +19,20 @@ public class ProjetoRepository{
             {
                 List<String> obra = new ArrayList<>();
                 obra.add("Obra 1");
+                obra.add("Obra 2");
                 List<String> tarefa = new ArrayList<>();
                 tarefa.add("Tarefa 1");
+                tarefa.add("Tarefa 2");
                 List<String> anexo = new ArrayList<>();
                 anexo.add("Projeto Arquitetônico");
                 List<String> equipe = new ArrayList<>();
                 equipe.add("A");
+                equipe.add("B");
                 List<String> orcamentoGeral = new ArrayList<>();
                 orcamentoGeral.add("20000");
                 List<String> orcamentoSimulacaoGeral = new ArrayList<>();
                 orcamentoSimulacaoGeral.add("17000");
+                orcamentoSimulacaoGeral.add("15000");
 
                 put(1, new Projeto(1, "Projeto Casa Moderna", "Casa", "Em execução",
                     "01/01/18", "01/12/18", obra, tarefa, anexo, equipe,
